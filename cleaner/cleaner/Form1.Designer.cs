aac70582f0d4de93c0526f56b91228515fc51b82
datanomi <datanomi@protonmail.com>
2017-03-08 18:54:53 +0200

    Cookie cleaner v.0.1

---
+++ cleaner/cleaner/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace cleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GET TREEVIEW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 37);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(281, 367);
+            this.treeView1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(389, 124);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(192, 226);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 545);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ cleaner/cleaner/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace cleaner
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void ListDirectory(TreeView treeView, string path)
+        //{
+        //    treeView.Nodes.Clear();
+        //    var rootdirinfo = new DirectoryInfo(path);
+        //    treeView.Nodes.Add(createDirectoryNode(rootdirinfo));
+
+        //}
+        //private static TreeNode createDirectoryNode(DirectoryInfo dirinfo)
+        //{
+        //    var directoryNode = new TreeNode(dirinfo.Name);
+        //    foreach (var directory in dirinfo.GetDirectories())
+
+        //        directoryNode.Nodes.Add(createDirectoryNode(directory));
+
+
+        //    foreach (var file in dirinfo.GetFiles())
+
+        //        directoryNode.Nodes.Add(new TreeNode(file.Name));
+        //    return directoryNode;
+        //}
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //ListDirectory(treeView1, @"C:\");
+
+
+            //this code is working
+
+            //only get dirs which name contains firefox string
+            string[] dirs = Directory.GetDirectories(@"D:\", "firefox*");
+            //loop throught D: drive
+            foreach (string subdir in dirs)
+            {
+                //load function which checks the sub folders
+                loadsubDirs(subdir);
+            }
+        }
+                
+               
+               
+
+
+            
+                private void loadsubDirs(string dir)
+        {
+
+            string[] subdirectoryEntries = Directory.GetDirectories(dir);
+            var files = new DirectoryInfo(dir).GetFiles("*");
+            //loop throught files.
+            foreach (var filename in files)
+            {
+
+                if (filename.ToString().Contains("AccessibleMarshal.dll"))
+                {
+                    richTextBox1.Controls.Clear();
+                    richTextBox1.Text += "\n" + dir + "\n" + filename.ToString();
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+            foreach (string subdirectory in subdirectoryEntries)
+
+            {
+
+                loadsubDirs(subdirectory);
+
+            }
+
+        }
+
+        }
+    }
+
+
+
